3650234caa7ef767d680acb50b50257f395c1a6b
cooper <cooper.yi@outlook.com>
2018-01-28 13:14:51 +0800

    basic judgement

--- Gobang_AI/GobangAI.cpp
+++ Gobang_AI/GobangAI.cpp
@@ -48,7 +48,33 @@ int placeStone(int x, int y, int kinds) {
 	return 0;
 }
 
-bool judgeResult() {
+bool judgeResult(int x, int y, int kinds) {
+	// row
+	int count = 0, lx = x - 1, rx = x + 1;
+	while (lx >= 0 && board[lx][y] == kinds) { --lx; ++count;}
+	while (rx < boardSize && board[rx][y] == kinds) { ++rx; ++count; }
+	if (count >= 4) return true;
+
+	// column
+	count = 0;
+	int uy = y - 1, dy = y + 1;
+	while (uy >= 0 && board[x][uy] == kinds) { --uy; ++count; }
+	while (dy < boardSize && board[x][dy] == kinds) { ++dy; ++count; }
+	if (count >= 4) return true;
+
+	// right-up / left-down
+	count = 0;
+	int rux = x + 1, ruy = y - 1, ldx = x - 1, ldy = y + 1;
+	while (rux < boardSize && ruy >= 0 && board[rux][ruy] == kinds) { ++rux; --ruy; ++count;}
+	while (ldx >= 0 && ldy < boardSize && board[ldx][ldy] == kinds) { --ldx; ++ldy; ++count;}
+	if (count >= 4) return true;
+
+	// left-up / right-down
+	count = 0;
+	int lux = x - 1, luy = y - 1, rdx = x + 1, rdy = y + 1;
+	while (lux >= 0 && luy >= 0 && board[lux][luy] == kinds) { --lux; --luy; ++count;}
+	while (rdx < boardSize && rdy < boardSize && board[rdx][rdy] == kinds) { ++rdx; ++rdy; ++count;}
+	if (count >= 4) return true;
 
 	return false;
 }
@@ -62,13 +88,26 @@ void holdGame() {
 	while (true)
 	{
 		tp = playStoneHuman(1);
-		placeStone(std::get<0>(tp), std::get<1>(tp), 1);
+		while (placeStone(std::get<0>(tp), std::get<1>(tp), 1)) {
+			std::cout << "illegal location, please reinput the black stone location!\n";
+			tp = playStoneHuman(1);
+		}
 		showBoard();
-		judgeResult();
+		if (judgeResult(std::get<0>(tp), std::get<1>(tp), 1)) {
+			std::cout << "Black Wins !!!!!\n";
+			break;
+		}
+
 		tp = playStoneHuman(-1);
-		placeStone(std::get<0>(tp), std::get<1>(tp), -1);
+		while (placeStone(std::get<0>(tp), std::get<1>(tp), -1)) {
+			std::cout << "illegal location, please reinput the white stone location!\n";
+			tp = playStoneHuman(-1);
+		}
 		showBoard();
-		judgeResult();
+		if (judgeResult(std::get<0>(tp), std::get<1>(tp), -1)) {
+			std::cout << "White Wins !!!!!\n";
+			break;
+		}
 	}
 }
 
@@ -76,6 +115,7 @@ void holdGame() {
 int main()
 {
 	holdGame();
+	system("pause");
     return 0;
 }
 
